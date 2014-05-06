4dae6c9dc6748aec820eef4a7626ec39005f5e1e
Roman Nikitin <roma@nikitin.me>
2014-05-06 11:34:11 -0700

    initial checkin

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('../lib/passport');
+
+// Redirect the user to Twitter for authentication.  When complete, Twitter
+// will redirect the user back to the application at
+//   /auth/twitter/callback
+router.get('/twitter', passport.authenticate('twitter'));
+
+// Twitter will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+router.get('/twitter/callback',
+passport.authenticate('twitter', { successRedirect: '/',
+									failureRedirect: '/login' }));
+
+module.exports = router;
